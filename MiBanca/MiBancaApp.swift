6a56007bf9d4cee996d04edd0bcbab50eef2e1b2
Americo Meneses <americo.meneses@infotec.mx>
2025-07-21 17:16:29 -0600

    feat: add baseproject

---
+++ MiBanca/MiBancaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiBancaApp.swift
+//  MiBanca
+//
+//  Created by Américo MQ on 21/07/25.
+//
+
+import SwiftUI
+
+@main
+struct MiBancaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
